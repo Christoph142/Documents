112c04bbf3b2a9642109b4266b7d0d98646f15bb
Christoph <Christoph142@myopera.com>
2012-10-31 17:39:35 +0100

    finished context menu

--- includes/rightclick.js
+++ includes/rightclick.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @include http://*
 // @include https://*
-// @exclude http://www.megalab.it/*
+// @exclude https://docs.google.com/viewer?*
+// @exclude http://acid3.acidtests.org*
 // ==/UserScript==
 
 //////////////////////////////////// Documents by Christoph142 ////////////////////////////////////
@@ -11,10 +12,31 @@
 //                                                                                               //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-var wanted_docs = new RegExp("\\.("+widget.preferences.wanted_docs+")$","gim");//$ = end of string
+var rightclick_docs = new RegExp("\\.("+widget.preferences.rightclick_docs+")$","gim");//$ = end of string
 
-window.addEventListener("DOMContentLoaded", function(){
-	if(opera.contexts.menu) opera.contexts.menu.onclick = function(menuEvent){
-		if(menuEvent.linkURL.match(wanted_docs)&&!menuEvent.linkURL.match(/\?/)) alert("");
-	};
-}, false);
+window.addEventListener("DOMContentLoaded", function(){ adjust_rightclick(document); }, false);
+window.addEventListener("DOMNodeInserted", function(){ adjust_rightclick(window.event.target); }, false);
+//window.oncontextmenu = function(event){ alert(event.currentTarget); alert(event.target); alert(event.relatedTarget); };
+function adjust_rightclick(param){
+	try{
+	all_links = param.getElementsByTagName("a");
+	for(var i=0; i < all_links.length; i++){
+		if(all_links[i].href.match(rightclick_docs) && (!all_links[i].href.match(/\?/) || all_links[i].href.match(/\?docex/))){
+			all_links[i].onmousedown = function(event){
+				if(event.which != 3) return; // only right-clicks
+				opera.extension.postMessage("add");
+			};
+		}
+		else{
+			all_links[i].onmousedown = function(event){
+				if(event.which != 3) return; // only right-clicks
+				opera.extension.postMessage("remove");
+			};
+		}
+	}
+	}catch(e){}
+}
+
+if(opera.contexts.menu) opera.contexts.menu.onclick = function(menuEvent){
+	window.location.href = (!menuEvent.linkURL.match(/\?docex/)?"https://docs.google.com/viewer?docex=1"+(widget.preferences.lang!="auto"?"&hl="+widget.preferences.lang:"")+"&url=":"")+menuEvent.linkURL;
+};
