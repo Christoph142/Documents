494012974af665820e4bb8b4d839dee6840eccb8
Christoph <Christoph142@myopera.com>
2012-12-08 17:23:26 +0100

    separate locales & code + manipulate stylesheets

--- includes/viewer.js
+++ includes/viewer.js
@@ -2,11 +2,25 @@
 // @include https://docs.google.com/viewer?docex=1&*
 // ==/UserScript==
 
-window.opera.addEventListener("BeforeEvent.DOMContentLoaded", function(){
-	
-	// set top to 28px to prevent the documents jumping around during page loading:
-	document.getElementById("content-pane").style.top = "28px";
-	document.getElementById("thumb-pane").style.top = "28px";
+window.opera.addEventListener("BeforeEvent", adjustCSS ,false);
+function adjustCSS(){
+	if(document.styleSheets){
+		var last = document.styleSheets.length-1;
+		var position_of_last_rule = document.styleSheets[document.styleSheets.length-1].cssRules.length;
+		
+		// set top to 28px to prevent the documents jumping around during page loading:
+		document.styleSheets[last].insertRule("#content-pane, #thumb-pane{ top:28px !important; }", position_of_last_rule);
+		document.styleSheets[last].insertRule("#controlbar{ position:fixed !important; top:28px !important; z-index:99 !important; display:none; transition:500ms; -o-transition:500ms; box-shadow:0 5px 10px -5px rgba(0,0,0,0.3); }", position_of_last_rule); // grey control bar
+		document.styleSheets[last].insertRule("#docex_titlediv{ float:right; height:22px; padding-top:5px; color:#666; transition:0.5s; -o-transition:0.5s;", position_of_last_rule);
+		document.styleSheets[last].insertRule("#menubar-shadow{ position:fixed; height:1px; width:100%; top:27px; left:0px; z-index:98; box-shadow:0 -2px 0 #FFF, 0 3px 5px rgba(0,0,0,0.45); border:none; border-bottom:1px solid #ccc; }", position_of_last_rule);
+		document.styleSheets[last].insertRule("#bugreport:hover, #rate_extension:hover{ background:#eee; }", position_of_last_rule);
+		
+		window.opera.removeEventListener("BeforeEvent", adjustCSS ,false);
+	}
+}
+
+window.opera.addEventListener("BeforeEvent.DOMContentLoaded", function()
+{
 	document.getElementById("content-pane").style.height = document.body.offsetHeight-28+"px";
 	document.getElementById("thumb-pane").style.height = document.body.offsetHeight-28+"px";
 	document.getElementById("thumb-pane-lower").style.height = document.body.offsetHeight-28+"px";
@@ -21,13 +35,12 @@ window.opera.addEventListener("BeforeEvent.DOMContentLoaded", function(){
 	document.getElementById("controlbar").offsetHeight = "28";
 	// add a fixed invisible div for the shadow of docs-menubar:
 	var docs_menubar_shadow = document.createElement("div");
-	docs_menubar_shadow.style = "position:fixed; height:1px; width:100%; top:27px; left:0px; z-index:98; box-shadow:0 -2px 2px rgba(255,255,255,0.9),0 3px 5px rgba(0,0,0,0.45); border:none; border-bottom:1px solid #ccc;";
+	docs_menubar_shadow.id = "menubar-shadow";
 	document.body.appendChild(docs_menubar_shadow);
 	
 	// display document's file name or its title (if available) at the right side of white docs-menubar:
 	var titlediv = document.createElement("div");
 	titlediv.id = "docex_titlediv";
-	titlediv.setAttribute("style","float:right; height:22px; padding-top:5px; color:#666; transition:0.5s; oTransition:0.5s;");
 	titlediv.innerHTML = document.title;
 	document.getElementById("docs-menubar").appendChild(titlediv);
 	window.setTimeout(check_title,1000); window.setTimeout(check_title,3000);
@@ -39,33 +52,21 @@ window.opera.addEventListener("BeforeEvent.DOMContentLoaded", function(){
 	document.getElementById(":h").outerHTML = "";
 	document.getElementById(":i").outerHTML = "";
 	document.getElementById(":m").id = "bugreport";
-	document.getElementById("bugreport").firstChild.innerHTML = "Report an issue";
+	document.getElementById("bugreport").firstChild.innerHTML = "Report an error";
 	document.getElementById("bugreport").onclick = function(){
 		window.open("https://addons.opera.com/extensions/details/documents/?reports#feedback-container"); };
-	document.getElementById("bugreport").onmouseover = function(){ this.style.backgroundColor = "#eee"; };
-	document.getElementById("bugreport").onmouseout = function(){ this.style.backgroundColor = "#fff"; };
 	document.getElementById(":n").id = "rate_extension";
 	document.getElementById("rate_extension").firstChild.innerHTML = "Rate Documents";
 	document.getElementById("rate_extension").onclick = function(){
 		window.open("https://addons.opera.com/extensions/details/documents/#feedback-container"); };
-	document.getElementById("rate_extension").onmouseover = function(){ this.style.backgroundColor = "#eee"; };
-	document.getElementById("rate_extension").onmouseout = function(){ this.style.backgroundColor = "#fff"; };
 	
-	// grey control bar:
-	document.getElementById("controlbar").style.position = "fixed"; 
-	document.getElementById("controlbar").style.top = "28px";
-	document.getElementById("controlbar").style.zIndex = "99";
-	document.getElementById("controlbar").style.display = "none";
-	document.getElementById("controlbar").style.transitionDuration = "500ms";
-	document.getElementById("controlbar").style.OTransitionDuration = "500ms";
-	document.getElementById("controlbar").style.boxShadow = "0 5px 5px rgba(0,0,0,0.2)";
-	// insert a button to save the documents:
+	// grey control bar: insert a button to save the documents:
 	var printbutton = document.getElementById("printToolbarButton");
 	var savebutton = printbutton.cloneNode(true);
 	savebutton.id = "saveToolbarButton";
 	if(document.URL.split("&docid")[0].match(extended_docs)){ // for extended functions selected:
-		savebutton.dataset.tooltip = "rightclick here and choose \"Save Linked Content as...\" to download this file";
-		savebutton.setAttribute("aria-label", "rightclick here and choose \"Save Linked Content as...\" to download this file");
+		savebutton.dataset.tooltip = "Rightclick here and choose \"Save Linked Content as...\" to download this file";
+		savebutton.setAttribute("aria-label", "Rightclick here and choose \"Save Linked Content as...\" to download this file");
 		savebutton.firstChild.firstChild.innerHTML = "<a href='"+document.URL.split("&url=")[1].split("&docid")[0]+"' style='cursor:default;' onclick='javascript:return false;'><img src='"+savebutton_src+"' height='29' style='margin-top:-2px;'></a>";
 	}
 	else{ //for basic mode:
@@ -83,7 +84,7 @@ window.opera.addEventListener("BeforeEvent.DOMContentLoaded", function(){
 	printbutton.parentNode.insertBefore(savebutton, printbutton);
 	printbutton.parentNode.insertBefore(spacer, printbutton);
 	// don't show print(PDF)-button if viewed file is a pdf or extended functions is active for PDFs:
-	if(document.getElementsByClassName("docs-title-inner")[0].innerHTML.match(new RegExp("(pdf)$","gi")) || widget.preferences.pdf == "2"){
+	if(document.getElementsByClassName("docs-title-inner")[0].innerHTML.match(new RegExp("\.pdf","i")) || widget.preferences.pdf == "3"){
 		document.getElementById("printToolbarButton").outerHTML = "";
 		document.getElementById("separator2").outerHTML = "";
 	}
@@ -103,57 +104,64 @@ window.opera.addEventListener("BeforeEvent.DOMContentLoaded", function(){
 	},1000);
 }, false);
 
-var controlbar_is_visible = 0;
+var controlbar_is_visible = 0; var controlbar_timeout;
 function adjust_controlbar(){
 	var e = window.event;
 	var active = document.activeElement.id;
 	if(!controlbar_is_visible && (e.pageY<29 || active=="searchBox")){
+		window.clearTimeout(controlbar_timeout);
 		document.getElementById("controlbar").style.display = "block";
 		document.getElementById("controlbar").style.opacity = "1";
+		document.getElementById("menubar-shadow").style.display = "none";
 		controlbar_is_visible = 1;
 	}
 	else if(controlbar_is_visible && ((e.keyCode==27 && active=="searchBox") || (active!="searchBox" && e.pageY>100))){
 		document.getElementById("controlbar").style.opacity = "0";
-		window.setTimeout(function(){document.getElementById("controlbar").style.display = "none";}, 500);
+		controlbar_timeout = window.setTimeout(function(){document.getElementById("controlbar").style.display = null;}, 500);
+		document.getElementById("menubar-shadow").style.display = null;
 		controlbar_is_visible = 0;
 	}
 }
 
 function localize_it(savebutton){
-	if(widget.preferences.lang == "de" || (widget.preferences.lang=="auto" && window.navigator.language=="de")){
-		document.getElementById("bugreport").firstChild.innerHTML = "Melde einen Fehler";
-		document.getElementById("rate_extension").firstChild.innerHTML = "Bewerte Documents";
+	var strings = {
+		"### DEFAULT: English ###" : {
+			"bugreport" : "Report an error",
+			"rate_extension" : "Rate Documents",
+			"Save file" : "Save file (Ctrl+S)",
+			"Save file rightclick" : "Rightclick here and choose \"Save Linked Content as...\" to download this file"
+		},
+		"de" : {
+			"bugreport" : "Melde einen Fehler",
+			"rate_extension" : "Bewerte Documents",
+			"Save file" : "Datei speichern (Ctrl+S)",
+			"Save file rightclick" : "Klicke hier mit der rechten Maustaste und wähle \"Verlinkten Inhalt speichern als...\" um die Datei herunterzuladen"
+		},
+		"fr" : {
+			"bugreport" : "Rapporte une erreur",
+			"rate_extension" : "Evalue Documents",
+			"Save file" : "Enregistre le fichier (Ctrl+S)",
+			"Save file rightclick" : "Fais un clic droit ici et choisis \"Enregistrer le contenu lié sous ... \" pour télécharger ce fichier"
+		},
+		"tr" : {
+			"bugreport" : "Hata bildir",
+			"rate_extension" : "Documents'i oyla",
+			"Save file" : "Dosyayı kaydet (Ctrl+S)",
+			"Save file rightclick" : "Dosyayı kaydetmek için buraya sağ tıklayın ve \"Hedefi farklı kaydet...\" i seçin"
+		}
+	};
+	var lang = widget.preferences.lang=="auto" ? window.navigator.language : widget.preferences.lang;
+	
+	if(strings[lang]){
+		document.getElementById("bugreport").firstChild.innerHTML = strings[lang]["bugreport"];
+		document.getElementById("rate_extension").firstChild.innerHTML = strings[lang]["rate_extension"];
 		if(savebutton.dataset.tooltip == "Save file (Ctrl+S)"){
-			savebutton.dataset.tooltip = "Datei speichern (Ctrl+S)";
-			savebutton.setAttribute("aria-label", "Datei speichern (Ctrl+S)");
+			savebutton.dataset.tooltip = strings[lang]["Save file"];
+			savebutton.setAttribute("aria-label", strings[lang]["Save file"]);
 		}
 		else {
-			savebutton.dataset.tooltip = "klicke hier mit der rechten Maustaste und wähle \"Verlinkten Inhalt speichern als...\" um die Datei zu speichern";
-			savebutton.setAttribute("aria-label", "klicke hier mit der rechten Maustaste und wähle \"Verlinkten Inhalt speichern als...\" um die Datei zu speichern");
-		}
-	}
-	else if(widget.preferences.lang == "fr" || (widget.preferences.lang=="auto" && window.navigator.language=="fr")){
-		document.getElementById("bugreport").firstChild.innerHTML = "Rapporte une erreur";
-		document.getElementById("rate_extension").firstChild.innerHTML = "Evalue Documents";
-		if(savebutton.dataset.tooltip == "Save file (Ctrl+S)"){
-			savebutton.dataset.tooltip = "Enregistre le fichier (Ctrl+S)";
-			savebutton.setAttribute("aria-label", "Enregistre le fichier (Ctrl+S)");
-		}
-		else {
-			savebutton.dataset.tooltip = "fais un clic droit ici et choisis \"Enregistrer le contenu lié sous ... \" pour télécharger ce fichier";
-			savebutton.setAttribute("aria-label", "fais un clic droit ici et choisis \"Enregistrer le contenu lié sous ... \" pour télécharger ce fichier");
-		}
-	}
-	else if(widget.preferences.lang == "tr" || (widget.preferences.lang=="auto" && window.navigator.language=="tr")){
-		document.getElementById("bugreport").firstChild.innerHTML = "Hata bildir";
-		document.getElementById("rate_extension").firstChild.innerHTML = "Documents\'i oyla";
-		if(savebutton.dataset.tooltip == "Save file (Ctrl+S)"){
-			savebutton.dataset.tooltip = "Dosyayı kaydet (Ctrl+S)";
-			savebutton.setAttribute("aria-label", "Dosyayı kaydet (Ctrl+S)");
-		}
-		else {
-			savebutton.dataset.tooltip = "dosyayı kaydetmek için buraya sağ tıklayın ve \"Hedefi farklı kaydet...\" i seçin";
-			savebutton.setAttribute("aria-label", "dosyayı kaydetmek için buraya sağ tıklayın ve \"Hedefi farklı kaydet...\" i seçin");
+			savebutton.dataset.tooltip = strings[lang]["Save file rightclick"];
+			savebutton.setAttribute("aria-label", strings[lang]["Save file rightclick"]);
 		}
 	}
 }
