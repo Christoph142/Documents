be511dc267e4b9b217ddb9c6c39c3aa4f10eb70c
Christoph <Christoph142@myopera.com>
2013-02-23 11:01:05 +0100

    make DOMNodeInserted-hack work with Opera 12.0X

--- includes/linkmanip.js
+++ includes/linkmanip.js
@@ -11,15 +11,24 @@
 //                                                                                               //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-var w = widget.preferences;
-var wanted_docs = new RegExp("^(?:[^\?]+\\.[^\?]+\\/[^\?]+\\.(?:"+w.wanted_docs+")((?:\\?|\\#).*)*)$","i"); // (no"?").(no"?")/(no"?").ENDING(optionally ?/#JUNK)*
-
-if(w.wanted_docs !== "") window.addEventListener("animationend", change_links, false);
-
-function change_links()
+(function()
 {
-	if(window.event.animationName !== "documents_linkInserted") return;
+	if(window.opera.version() < 12) return;
 	
-	var l = window.event.target.href;
-	if(l.match(wanted_docs)) window.event.target.href = "https://docs.google.com/viewer?docex=1"+(w.lang!=="auto"?"&hl="+w.lang:"")+"&url="+l;
-}
+	var w = widget.preferences;
+	var wanted_docs = new RegExp("^(?:[^\?]+\\.[^\?]+\\/[^\?]+\\.(?:"+w.wanted_docs+")((?:\\?|\\#).*)*)$","i"); // (no"?").(no"?")/(no"?").ENDING(optionally ?/#JUNK)*
+	
+	if(w.wanted_docs !== "")
+	{
+		window.addEventListener("oAnimationEnd", change_links, false);	// Opera 12.0X
+		window.addEventListener("animationEnd", change_links, false);	// Opera 12.10+
+	}
+	
+	function change_links()
+	{
+		if(window.event.animationName !== "documents_linkInserted") return;
+		
+		var l = window.event.target.href;
+		if(l.match(wanted_docs)) window.event.target.href="https://docs.google.com/viewer?docex=1"+(w.lang!=="auto"?"&hl="+w.lang:"")+"&url="+l;
+	}
+})();
