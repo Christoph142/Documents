ab2698e0eec8b96b9c2c53cb4e128e1d03dc253d
Christoph <christoph142@myopera.com>
2013-07-04 13:09:10 +0200

    treat links inserted via Ajax

--- Chromium (.crx)/includes/linkmanip.js
+++ Chromium (.crx)/includes/linkmanip.js
@@ -4,35 +4,33 @@
 	
 	chrome.extension.sendMessage({data:"settings"}, function(response){ // get settings (filled with default values) from background.js
 		w = response;
-		window.addEventListener("DOMContentLoaded", change_links, false);
+		if(w.wanted_docs !== "") init_documents();
 	});
 	
-	function change_links()
+	function init_documents()
 	{
 		var wanted_docs = new RegExp("^(?:[^\?]+\\.[^\?]+\\/[^\?]+\\.(?:"+w.wanted_docs+")((?:\\?|\\#).*)*)$","i"); // (no"?").(no"?")/(no"?").ENDING(optionally ?/#JUNK)*
-	
-		if(w.wanted_docs !== "")
-		{
-			var links = document.getElementsByTagName("a");
-			for(var i = 0; i < links.length; i++)
-			{
-				if(links[i].href.match(wanted_docs))
-				{
-					var l = links[i].href;
-					links[i].href = "https://docs.google.com/viewer?docex=1&url="+l;
-				}
-			}
-			
-			var DOM_observer = new WebKitMutationObserver(change_links);
-			DOM_observer.observe(document.body, { childList:true, subtree:true });
-		}
+		
+		// DOMNodeInserted-hack by Daniel Buchner (http://www.backalleycoder.com/2012/04/25/i-want-a-damnodeinserted)
+		window.addEventListener("DOMContentLoaded", function(){
+			var style = document.createElement("style");
+			style.setAttribute("type","text/css");
+			style.id = "Documents_extension_style";
+			style.innerHTML = "@keyframes documents_linkInserted { from{ clip:rect(1px, auto, auto, auto); }to{ clip:rect(0px, auto, auto, auto); } }"+
+			"@-webkit-keyframes documents_linkInserted { from{ clip:rect(1px, auto, auto, auto); }to{ clip:rect(0px, auto, auto, auto); } }"+
+			"a { animation: documents_linkInserted 1ms; -webkit-animation: documents_linkInserted 1ms; }";
+			document.getElementsByTagName("head")[0].appendChild(style);
+		}, false);
+
+		window.addEventListener("animationEnd", change_links, false);
+		window.addEventListener("webkitAnimationEnd", change_links, false);
 		
 		function change_links()
 		{
-			return;
+			if(window.event.animationName !== "documents_linkInserted") return;
 			
 			var l = window.event.target.href;
 			if(l.match(wanted_docs)) window.event.target.href="https://docs.google.com/viewer?docex=1&url="+l;
-		}	
+		}
 	}
 })();
